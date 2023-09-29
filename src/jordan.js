e7981e77e6fe2698de2337d24c7a9b7ba0370f31
Ritesh Nandy <ritesh.nandy2002@gmail.com>
2023-09-29 15:59:39 +0530

    v2.10

--- src/jordan.js
+++ src/jordan.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import img1 from './images/about.jpg'
 
 const jordan = () => {
+  const backbr = {
+    position: 'absolute',
+    top: '50px',
+    left: '50px',
+  };
+    
   return (
-    <div>
-      
-    </div>
+    <section className="home" id="home">
+      <div style={backbr}>
+      <a href="#" className="btn">Back</a>
+      </div>
+      <div className="home-content">
+        <h3>Air Jordan <span className="multiple-text">1 Mid SE</span></h3>
+        <h1>Zen Master</h1>
+        <h3><span className="multiple-text">Minting </span>Now</h3>
+        <p>Price: 0.18 ETH Tokens</p>
+        <br/>
+        <br/>
+        <a href="#" className="btn">Mint</a>
+      </div>
+
+      <div class="home-img">
+        <img src={img1}/>
+      </div>
+
+    </section>
   )
 }
 
-export default jordan
+export default jordan 
