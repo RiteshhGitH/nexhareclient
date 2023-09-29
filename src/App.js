fca6eddbab68b41a071e9672dcf40e47d06b99d1
Ritesh Nandy <ritesh.nandy2002@gmail.com>
2023-09-29 16:06:59 +0530

    V2.1.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import {BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./Home"
-import Jordan from "./Jordan"
+import Jordan from "./jordan"
 const App = () => {
   return (
     <BrowserRouter>
